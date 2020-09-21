7f318986c929e0dbba2838f0472496a8fe29b18d
Alexander Fraltsov <kolonessens@yandex.ru>
2020-09-21 22:29:16 +0400

    feat: worklog-tables integrated into worklog-page

--- src/app/modules/worklog/worklog.module.ts
+++ src/app/modules/worklog/worklog.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { WorklogComponent } from './worklog/worklog.component';
 import { SharedModule } from '../../shared/shared.module';
+import { WorklogTableComponent } from './components/worklog-table/worklog-table.component';
 
 @NgModule({
-  declarations: [WorklogComponent],
+  declarations: [WorklogComponent, WorklogTableComponent],
   imports: [
     SharedModule
   ]
--- src/app/modules/worklog/worklog/worklog.component.ts
+++ src/app/modules/worklog/worklog/worklog.component.ts
@@ -1,5 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IWorklogItem } from '../models/worklog-item.model';
+
+const ELEMENTS: IWorklogItem[] = [
+  {
+    feature: 'feature',
+    timeSpent: 1,
+    startDate: new Date('2020-09-20'),
+    endDate: new Date('2020-09-20'),
+  }
+];
+
+const enum developers {
+  ASADCHY = 'Pavel Asadchy',
+  FRALTSOV = 'Alexander Fraltsov',
+  RUDNIK = 'Mikita Rudnik',
+  SIDOROV = 'Alexandr Sidorov',
+  STEPOVENKO = 'Valentyn Stepovenko'
+}
+
+const MOCK_DATA = [
+  {
+    user: developers.ASADCHY,
+    features: ELEMENTS
+  },
+  {
+    user: developers.FRALTSOV,
+    features: ELEMENTS
+  },
+  {
+    user: developers.RUDNIK,
+    features: ELEMENTS
+  },
+  {
+    user: developers.SIDOROV,
+    features: ELEMENTS
+  },
+  {
+    user: developers.STEPOVENKO,
+    features: ELEMENTS
+  }
+];
+
 @Component({
   selector: 'app-worklog',
   templateUrl: './worklog.component.html',
@@ -7,9 +49,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorklogComponent implements OnInit {
 
+  public tableData = MOCK_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
