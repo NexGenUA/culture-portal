3cafa1d43b90c1b71cd94f0c3d71142aa59ec969
pavel asadchy <asadchy.pavel@gmail.com>
2020-09-23 00:12:45 +0300

    feat: main page & cards

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MainComponent } from './pages/main/main.component';
 import { SharedModule } from '../../shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, HomeComponent, CardComponent],
   imports: [
     SharedModule
   ]
