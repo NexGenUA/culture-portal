104c84f889ca72234bcd8b9093f74917d52e1080
Alexander Fraltsov <kolonessens@yandex.ru>
2020-09-25 21:46:11 +0400

    feat: this.writers changes on input request to search input

--- src/app/modules/search/pages/search/search.component.ts
+++ src/app/modules/search/pages/search/search.component.ts
@@ -1,15 +1,58 @@
+import { IWritter } from './../../../../shared/models/writer.model';
+import { HttpService } from './../../../../shared/services/http.service';
+import { filter, debounceTime } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
+const QUERY_MIN_LENGTH = 1;
+const DEBOUNCE_TIME = 1000;
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  public value = 'clear me';
-  constructor() { }
+
+  public form: FormGroup;
+  public searchInstance: 'name' | 'address' = 'name';
+  public value = '';
+  public searchString: Subscription;
+  public writers: IWritter[] = [];
+
+  constructor(public httpService: HttpService) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      searchString: new FormControl('searchString')
+    });
+
+    this.httpService.getFullBase().then(writers => {
+      this.writers = writers;
+    });
+
+    this.searchString = this.form.get('searchString').valueChanges
+    .pipe(
+      filter(s => s.length >= QUERY_MIN_LENGTH),
+      debounceTime(DEBOUNCE_TIME)
+      )
+    .subscribe(
+      query => {
+        this.httpService
+          .getCardByName(query)
+          .then(writers => {
+            this.writers = writers;
+          });
+      }
+    );
+  }
+
+  public toggleSearchInstance = () => {
+    if (this.searchInstance === 'name') {
+      this.searchInstance = 'address';
+    } else {
+      this.searchInstance = 'name';
+    }
   }
 
 }
