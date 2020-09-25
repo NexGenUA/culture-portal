d3e00dc88ad80c0c87ca35aeb2867c91cb9de52e
Alexander Fraltsov <kolonessens@yandex.ru>
2020-09-25 20:47:31 +0400

    feat: added search input to authors list page

--- src/app/modules/search/pages/search/search.component.ts
+++ src/app/modules/search/pages/search/search.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  public value = 'clear me';
   constructor() { }
 
   ngOnInit(): void {
